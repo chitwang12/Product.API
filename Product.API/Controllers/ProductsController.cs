dcfb10f569169a686a17c503a362651c42a452b3
chitwang12 <chitwangandhi12@gmail.com>
2021-09-17 00:31:49 +0530

    last commit with message

--- Product.API/Controllers/ProductsController.cs
+++ Product.API/Controllers/ProductsController.cs
@@ -24,6 +24,9 @@ namespace Product.API.Controllers
         [HttpPost]
         public async Task<IActionResult> addProductRating([FromRoute] ProductModel productModel)
         {
+            //bro maine code likh toh diya par yeh jo _productRepository hai yeh tere system main hogaa generated toh vahan se uthaalega yeh 
+          //  subah uth kar tu ek baar yeh code apne project main daal lena chal jayega yeh 200% nahi chalega toh dekhengey phir kuch aur hisaab 
+          //Baaki yeh ho gaya hai ab meri side se .
             var id = await _productRepository.AddProductAsync(productModel);
             return CreatedAtAction(nameof(searchProductById), new { id = id, controller = "Products" }, id);
         }
